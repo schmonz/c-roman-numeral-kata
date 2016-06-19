5e22cc9f444bc6c5de9cb24c73c7000eabd5e8f1
Amitai Schlair <schmonz-web-git@schmonz.com>
2016-06-19 13:51:51 -0400

    Use better assertions for better failures.

--- check_roman_numeral_calculator.c
+++ check_roman_numeral_calculator.c
@@ -84,34 +84,34 @@ static const char * roman_add(const char *roman1, const char *roman2) {
 */
 
 START_TEST(test_invalid_roman_to_arabic) {
-    ck_assert(INVALID_ROMAN_NUMERAL == roman_to_arabic("R"));
-    ck_assert(INVALID_ROMAN_NUMERAL == roman_to_arabic("VIIR"));
+    ck_assert_int_eq(INVALID_ROMAN_NUMERAL, roman_to_arabic("R"));
+    ck_assert_int_eq(INVALID_ROMAN_NUMERAL, roman_to_arabic("VIIR"));
 } END_TEST
 
 START_TEST(test_valid_roman_digits_to_arabic) {
-    ck_assert(1 == roman_to_arabic("I"));
-    ck_assert(5 == roman_to_arabic("V"));
-    ck_assert(10 == roman_to_arabic("X"));
-    ck_assert(50 == roman_to_arabic("L"));
-    ck_assert(100 == roman_to_arabic("C"));
-    ck_assert(500 == roman_to_arabic("D"));
-    ck_assert(1000 == roman_to_arabic("M"));
+    ck_assert_int_eq(1, roman_to_arabic("I"));
+    ck_assert_int_eq(5, roman_to_arabic("V"));
+    ck_assert_int_eq(10, roman_to_arabic("X"));
+    ck_assert_int_eq(50, roman_to_arabic("L"));
+    ck_assert_int_eq(100, roman_to_arabic("C"));
+    ck_assert_int_eq(500, roman_to_arabic("D"));
+    ck_assert_int_eq(1000, roman_to_arabic("M"));
 } END_TEST
 
 START_TEST(test_additively_constructed_roman_numerals) {
-    ck_assert(2 == roman_to_arabic("II"));
-    ck_assert(8 == roman_to_arabic("VIII"));
+    ck_assert_int_eq(2, roman_to_arabic("II"));
+    ck_assert_int_eq(8, roman_to_arabic("VIII"));
 } END_TEST
 
 START_TEST(test_subtractively_constructed_roman_numerals) {
-    ck_assert(4 == roman_to_arabic("IV"));
-    ck_assert(1990 == roman_to_arabic("MCMXC"));
-    ck_assert(47 == roman_to_arabic("XLVII"));
+    ck_assert_int_eq(4, roman_to_arabic("IV"));
+    ck_assert_int_eq(1990, roman_to_arabic("MCMXC"));
+    ck_assert_int_eq(47, roman_to_arabic("XLVII"));
 } END_TEST
 
 START_TEST(test_arabic_to_roman) {
-    ck_assert((0 == strcmp(arabic_to_roman(1), "I")));
-    ck_assert((0 == strcmp(arabic_to_roman(6), "VI")));
+    ck_assert_str_eq(arabic_to_roman(1), "I");
+    ck_assert_str_eq(arabic_to_roman(6), "VI");
 } END_TEST
 
 START_TEST(test_add_two_roman_numerals) {
