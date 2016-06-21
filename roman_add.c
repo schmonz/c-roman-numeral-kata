f4a9af86d53a69a90705b6760a18f28d3e229cad
Amitai Schlair <schmonz-web-git@schmonz.com>
2016-06-20 22:44:45 -0400

    roman_add two numerals from the command line.

--- roman_add.c
+++ roman_add.c
@@ -1,7 +1,13 @@
+#include <libgen.h>
 #include <stdio.h>
 #include "roman_calculator.h"
 
-int main(void) {
-    printf("%s\n", roman_add("XIV", "LX"));
-    return 0;
+int main(int argc, char *argv[]) {
+    if (argc == 3) {
+        printf("%s\n", roman_add(argv[1], argv[2]));
+        return 0;
+    } else {
+        printf("usage: %s <roman-numeral> <roman-numeral>\n", basename(argv[0]));
+        return 77;
+    }
 }
