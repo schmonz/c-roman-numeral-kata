b2e1d4aa9a454d7db70413bd3e6aa944a11627dc
Amitai Schlair <schmonz-web-git@schmonz.com>
2016-06-20 23:13:42 -0400

    Tolerate being included multiple times.

--- roman_calculator.h
+++ roman_calculator.h
@@ -1,3 +1,6 @@
+#ifndef ROMAN_CALCULATOR_H
+#define ROMAN_CALCULATOR_H
+
 #define INVALID_ROMAN_NUMERAL -1
 
 const int    roman_digit_to_arabic(const char);
@@ -8,3 +11,5 @@ const char * arabic_to_roman(int);
 
 const char * roman_add(const char *, const char *);
 const char * roman_subtract(const char *, const char *);
+
+#endif
