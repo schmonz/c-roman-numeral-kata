6c0677a9516744a0e688a827aea8a22029a3e3d7
Amitai Schlair <schmonz-web-github@schmonz.com>
2016-06-22 14:11:13 -0400

    Comments from someone who knows C

--- roman_calculator.c
+++ roman_calculator.c
@@ -5,6 +5,17 @@
 
 /*
  * TODO:
+ * instead of casting free(roman), stop const-ing it
+ * size_t (or unsigned) for array indices
+ * check for malloc() failure
+ * _build_up_roman(char **romanp) and _build_up_roman(&roman)
+ * _build_up_roman() -> append_to_roman()
+ * malloc() enough space for a trailing '\0'
+ * that malloc(0) should be malloc(1), and put a '\0' in it
+ * replace the repeated malloc()/strlcpy() with realloc()
+ * replace strcpy()/strcat() with strlcpy()/strlcat()
+ * no need for const on functions that return int (because const does nothing for rvalues, only lvalues)
+ * for reverse iteration, idiomatic, memorable, and safe: for (i = maximum_value; i-- > 0;)
  * test more edge cases
  * reduce more duplication
  * try on Ubuntu with gcc, fixing whatever breaks
