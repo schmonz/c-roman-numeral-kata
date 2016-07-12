3e48476367fcf63bf56c1ae14bc656673c33af07
Amitai Schlair <schmonz-web-git@schmonz.com>
2016-07-12 13:24:14 -0400

    Acceptance tests are for numbers, not numerals.

--- check_roman_calculator_acceptance.c
+++ check_roman_calculator_acceptance.c
@@ -2,25 +2,25 @@
 
 #include "roman_calculator.h"
 
-START_TEST(test_add_invalid_roman_numerals) {
+START_TEST(test_add_invalid_roman_numbers) {
     ck_assert_str_eq("INVALID", roman_add("V", "R"));
 } END_TEST
 
-START_TEST(test_add_two_roman_numerals) {
+START_TEST(test_add_two_roman_numbers) {
     ck_assert_str_eq("LXXIV", roman_add("XIV", "LX"));
     ck_assert_str_eq( "XXII", roman_add( "XX", "II"));
 } END_TEST
 
-START_TEST(test_subtract_two_roman_numerals) {
+START_TEST(test_subtract_two_roman_numbers) {
     ck_assert_str_eq("XLVI", roman_subtract("LX", "XIV"));
 } END_TEST
 
 TCase* tcase_acceptance(void) {
     TCase *tc = tcase_create("Acceptance Tests");
 
-    tcase_add_test(tc, test_add_invalid_roman_numerals);
-    tcase_add_test(tc, test_add_two_roman_numerals);
-    tcase_add_test(tc, test_subtract_two_roman_numerals);
+    tcase_add_test(tc, test_add_invalid_roman_numbers);
+    tcase_add_test(tc, test_add_two_roman_numbers);
+    tcase_add_test(tc, test_subtract_two_roman_numbers);
 
     return tc;
 }
